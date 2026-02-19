f998b1ec66773fa84ee397179cb8531bd901a68e
Tani <tani.9kuz@gmail.com>
2026-02-19 14:35:23 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,3 @@
 public class Main {
-    public static void main(String[] args){ System.out.println("Hello and welcome!"); }
+    public static void main(String[] args){ System.out.println("Привет"); }
 }
